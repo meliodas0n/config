49d90a595b37413bc63ad091673919c86159a24f
meliodas0n <mrunalnarayana@gmail.com>
2023-05-08 21:23:58 +0530

    added ranger with dependencies

---
+++ install_ranger.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo pacman -S atool elinks ffmpegthumbnailer highlight imagemagick libcaca lynx mediainfo odt2txt perl-image-exiftool poppler python-chardet python-pillow transmission-cli ueberzug w3m ranger
