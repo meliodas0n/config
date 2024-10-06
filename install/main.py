c14280904483b5f78190f8e7793d3ab6581ae968
meliodas0n <mrunalnarayana@gmail.com>
2024-10-06 18:58:32 +0000

    some old code

--- install/main.py
+++ install/main.py
@@ -1,12 +1,12 @@
 import os
-# from subprocess import call
+from subprocess import call
 
 for i in os.listdir("."):
   if not i.endswith(".xml"):
     os.chmod(i, 0o755)
 
-# for i in os.listdir("."):
-#   if not i.endswith(".xml"):
-#     print(f'chmod +x {i}')
-#     call(f'chmod +x {i}')
-#     print(f"execute permission granted for {i}")
+for i in os.listdir("."):
+  if not i.endswith(".xml"):
+    print(f'chmod +x {i}')
+    call(f'chmod +x {i}')
+    print(f"execute permission granted for {i}")
