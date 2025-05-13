5b147c690ce1eb17bfb8891a2433448c01e4128b
meliodas0n <mrunalnarayana@gmail.com>
2025-05-14 05:23:18 +0530

    started writing arch and ubuntu install

--- autosetup.sh
+++ autosetup.sh
@@ -3,9 +3,32 @@ set -e
 
 CONFIG_FOLDER=$HOM/config
 
-echo "Updating the System First"
-sudo apt-get update && sudo apt full-upgrade && sudo apt dist-upgrade -y
-echo "System upgrade completed"
+ubuntu_install() {
+  echo "Updating the System First"
+  sudo apt-get update && sudo apt full-upgrade && sudo apt dist-upgrade -y
+  echo "System upgrade completed"
 
-. install/nvim_install
-. install/tmux_install
+  . install/nvim_install
+  . install/tmux_install
+}
+
+arch_install() {
+  echo "Arch install started $(date)"
+}
+
+install() {
+  echo "Starting auto setup of the tools"
+  echo "passed distro is $DISTRO"
+  if [ "$DISTRO"  = "Ubuntu" ]; then
+    ubuntu_install
+  elif [ "$DISTRO" = 'Arch' ]; then
+    arch_install
+  fi
+}
+
+if [ $# -ne 1 ]; then
+  echo "Invalid distribution or wrong arguments"
+else
+  DISTRO=$1
+  install
+fi
